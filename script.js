ef6fbd00a5a9dea6cb77dcf81830092a629cb3a8
Abhay Sharma <hi@abhaysharma.in>
2016-08-29 01:09:26 +0530

    TaxBot
    TaxBot

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Abhay? Just say HELLO to get started.')
+            return bot.say('So you want to file your income tax return? Just say HELLO to get started.')
                 .then(() => 'speak');
         }
     },
